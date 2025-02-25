f530e9a44ae7fe5ceaa8a6f8102561870459d316
Reveal Ai UG <wael.alkhatib@revealai.de>
2025-02-25 10:44:19 +0000

    Setup django project

---
+++ gen_flow/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (C) 2024 Reveal AI
+#
+# SPDX-License-Identifier: MIT
+
+from gen_flow.utils.version import get_version
+
+VERSION = (1, 0, 0, 'alpha', 0)
+
+__version__ = get_version(VERSION)
