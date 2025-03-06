f41526d63c88e9ea1332274a285cda2a345273a4
Reveal Ai UG <wael.alkhatib@revealai.de>
2025-03-06 13:11:40 +0000

    Added team app

---
+++ gen_flow/apps/team/apps.py
@@ -0,0 +1,11 @@
+# Copyright (C) 2024 Reveal AI
+#
+# SPDX-License-Identifier: MIT
+
+from django.apps import AppConfig
+
+class TeamConfig(AppConfig):
+    name = 'gen_flow.apps.team'
+
+    def ready(self):
+        pass
