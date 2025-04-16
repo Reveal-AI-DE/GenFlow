d76a1c3cea87803d22bc7ce436dcd93b068406ab
Reveal Ai UG <wael.alkhatib@revealai.de>
2025-04-16 07:35:13 +0000

    minor fix

--- manage.py
+++ manage.py
@@ -4,7 +4,10 @@ import os
 import sys
 
 if __name__ == "__main__":
-    """Run administrative tasks."""
+    """
+    Run administrative tasks.
+    """
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gen_flow.settings.development")
     try:
         from django.core.management import execute_from_command_line
